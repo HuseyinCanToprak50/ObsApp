350abfc78407dd9129ccd6fd5fb3526b7c1408fb
HuseyinCanToprak50 <mecan38_@hotmail.com>
2024-12-29 14:22:58 +0300

    Add project files.

---
+++ Migrations/20241227164523_UpdateInitDbObs.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ObsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateInitDbObs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OgrenciDersler",
+                table: "OgrenciDersler",
+                column: "TempKey");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OgrenciDersler",
+                table: "OgrenciDersler");
+        }
+    }
+}
